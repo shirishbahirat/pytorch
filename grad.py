6b6f5eb2f1606f494c583dbee1ff7e484437a591
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-28 07:52:52 -0700

    test commit

--- grad.py
+++ grad.py
@@ -1,21 +1,24 @@
-import torch 
+import torch
 import torchvision
 import torch.nn as nn
 import numpy as np
 import torchvision.transforms as transforms
 
-# Create tensors.
 x = torch.tensor(1., requires_grad=True)
 w = torch.tensor(2., requires_grad=True)
 b = torch.tensor(3., requires_grad=True)
 
-# Build a computational graph.
-y = w * x + b    # y = 2 * x + 3
+print(x)
+print(w)
+print(b)
 
-# Compute gradients.
+
+y = w * x + b
+
+print(y)
+
 y.backward()
 
-# Print out the gradients.
-print(x.grad)    # x.grad = 2 
-print(w.grad)    # w.grad = 1 
-print(b.grad)    # b.grad = 1 
+print(x.grad)    # x.grad = 2
+print(w.grad)    # w.grad = 1
+print(b.grad)    # b.grad = 1

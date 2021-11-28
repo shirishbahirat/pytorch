9f3d5b596aa1cf5776d523dcd76ed05fcff81064
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-28 14:25:34 -0700

    test commit to learn pytorch

--- lin_regs.py
+++ lin_regs.py
@@ -6,6 +6,9 @@ w = 1.0
 w_list = []
 mse_list = []
 
+x_data = [1., 2., 3.]
+y_data = [2., 4., 6.]
+
 
 def forward(x):
     return x * w
@@ -16,22 +19,20 @@ def loss(x, y):
     return (err * err)
 
 
-def mse():
+for w in np.arrange(0., 4.1, .1):
+    print ("w: ", w)
+    mse = 0
+    lsum = 0
+    for x_val, y_val in zip(x_data, y_data):
+        y_pred = forward(x_val)
+        l = loss(x_val, y_val)
+        lsum += l
+        print("\t", x_val, y_val, y_pred, l)
+        mse = lsum / 3
+    print("MSE:", mse)
 
-    for w in np.arrange(0., 4.1, .1):
-        print ("w: ", w)
-        mse = 0
-        lsum = 0
-        for x_val, y_val in zip(x_data, y_data):
-            y_pred = forward(x_val)
-            l = loss(x_val, y_val)
-            lsum += l
-            print("\t", x_val, y_val, y_pred, l)
-            mse = lsum / 3
-        print("MSE:", mse)
-
-        w_list.append(w)
-        mse_list.append(mse)
+    w_list.append(w)
+    mse_list.append(mse)
 
 
 plt.plot(w_list, mse_list)

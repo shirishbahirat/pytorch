653d2460cfc027a65b3b3ee248fcfc60bbeb378b
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-28 18:41:29 -0700

    test commit to learn pytorch

--- torch_grad.py
+++ torch_grad.py
@@ -11,7 +11,7 @@ y_data = Variable(torch.tensor([[2.], [4.], [6.]]))
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.linear = torch.nn.linear(1, 1)
+        self.linear = torch.nn.Linear(1, 1)
 
     def forward(self, x):
         y_pred = self.linear(x)
@@ -19,3 +19,22 @@ class Model(torch.nn.Module):
 
 
 model = Model()
+
+
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(500):
+
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+hr = Variable(torch.tensor([4.]))
+model.forward(hr)
+
+print ("predict (after training)", 4, model.forward(hr).item())

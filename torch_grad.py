6cf96feec824171027e04b8df7436589391cda95
Shirish Bahirat <shirishbahirat@comcast.net>
2021-11-28 18:28:08 -0700

    test commit to learn pytorch

--- torch_grad.py
+++ torch_grad.py
@@ -11,6 +11,7 @@ y_data = Variable(torch.tensor([[2.], [4.], [6.]]))
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
+        self.linear = torch.nn.linear(1, 1)
 
     def forward(self, x):
         y_pred = self.linear(x)

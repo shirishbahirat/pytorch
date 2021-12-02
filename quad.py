a00bf2771de6ffca1d15e886eafea11220ab526a
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-01 19:19:48 -0700

    test commit to learn pytorch

--- quad.py
+++ quad.py
@@ -6,13 +6,15 @@ import torchvision.transforms as transforms
 
 a = torch.tensor(6., requires_grad=True)
 b = torch.tensor(7., requires_grad=True)
-c = torch.tensor(7., requires_grad=True)
+c = torch.tensor(5., requires_grad=True)
+d = torch.tensor(2., requires_grad=True)
 
 print(a)
 print(b)
 print(c)
+print(d)
 
-y = a**3 + b**2 + c
+y = a**3 + b**2 + c**1 + d
 
 print(y)
 
@@ -21,3 +23,4 @@ y.backward()
 print(a.grad)
 print(b.grad)
 print(c.grad)
+print(d.grad)

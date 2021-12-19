78a7dc62c7f8959841ca5263f7ce1dec4a21bbe4
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-19 09:11:38 -0700

    test commit to learn pytorch

--- gd.py
+++ gd.py
@@ -3,4 +3,20 @@ import numpy as np
 
 points = [(np.array([4]), 2), (np.array([2]), 4)]
 
-points
+d = 1
+
+
+def F(w):
+    return sum((w.dot(x) - y)**2 for x, y in points)
+
+
+def dF(w):
+    return sum(2 * (w.dot(x) - y) * x for x, y in points)
+
+
+def gradientDecent(F, dF, d):
+    w = np.zeros(d)
+    eta = 0.001
+    for t in range(100):
+        value = F(w)
+        gradient = dF(w)

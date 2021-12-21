4aa4553f5bbb83b0e1053cda6f3259561955be97
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-20 21:31:07 -0700

    test commit to learn pytorch

--- qemu.py
+++ qemu.py
@@ -4,3 +4,13 @@ nvme list
 
 nvme ns - descs / dev / nvme0n1
 nvme ns - descs / dev / nvme0n2
+
+nvme zns id - ns / dev / nvme0n2
+
+nvme zns report - zones / dev / nvme0n2 - s 0x0 - d 3
+
+nvme write / dev / nvme0n2 - s 0x0 - d 3
+
+nvme zns report - zones / dev / nvme0n2 - s 0x0 - d 3
+
+ls / dev/

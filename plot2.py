bb6126494a24e83561b150f74296b7ff0c56b96e
Shirish Bahirat <shirishbahirat@comcast.net>
2021-12-29 08:43:38 -0700

    test commit to learn pytorch

--- plot2.py
+++ plot2.py
@@ -7,7 +7,7 @@ barWidth = 0.25
 
 # set heights of bars
 
-size = 15
+size = 20
 
 plt.rc('font', size=size)
 plt.rc('axes', titlesize=size)
@@ -27,16 +27,16 @@ r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 
 
-def plot_data(name, dqos):
+def plot_data(name, dqos, xlbl, ylbl):
     plt.bar(r1, dqos[2], color='#5d7f5e', width=barWidth, edgecolor='white', label='End to end priority')
     plt.bar(r2, dqos[1], color='#85af2d', width=barWidth, edgecolor='white', label='Controlled forwarding')
     plt.bar(r3, dqos[0], color='#a6d512', width=barWidth, edgecolor='white', label='No priority')
 
-    plt.xlabel('Latency Percentiles', fontweight='bold')
-    plt.ylabel('QoS [us]', fontweight='bold')
+    plt.xlabel(xlbl, fontweight='bold')
+    plt.ylabel(ylbl, fontweight='bold')
     plt.xticks([r + barWidth for r in range(len(dqos[0]))], ['0.5', '0.75', '1 9', '2 9', '3 9', '4 9', '5 9', '6 9'])
 
-    plt.title(name, y=1.0, pad=-100)
+    plt.title(name, y=1.0, pad=-130)
 
     plt.legend()
 
@@ -44,7 +44,7 @@ def plot_data(name, dqos):
 fig = plt.figure()
 ax = fig.add_subplot(221)
 
-plot_data('(a) High Priority', domain1_qos)
+plot_data('(a) High Priority', domain1_qos, '', 'QoS [us]')
 
 
 domain2_qos = [[293, 355, 412, 578, 742, 930, 1385, 3130],
@@ -52,14 +52,14 @@ domain2_qos = [[293, 355, 412, 578, 742, 930, 1385, 3130],
                [128, 196, 273, 482, 701, 914, 1139, 1352]]
 
 ax = fig.add_subplot(222)
-plot_data('(b) Mid Priority', domain2_qos)
+plot_data('(b) Mid Priority', domain2_qos, '', '')
 
 domain3_qos = [[293, 355, 412, 578, 742, 930, 1385, 3130],
                [627, 668, 717, 873, 1037, 1205, 1385, 1582],
                [165, 237, 347, 644, 955, 1287, 1647, 2278]]
 
 ax = fig.add_subplot(223)
-plot_data('(c) Low Priority', domain3_qos)
+plot_data('(c) Low Priority', domain3_qos, 'Latency Percentiles', 'QoS [us]')
 
 
 domain4_qos = [[293, 355, 408, 578, 742, 930, 1385, 5014],
@@ -67,7 +67,7 @@ domain4_qos = [[293, 355, 408, 578, 742, 930, 1385, 5014],
                [192, 281, 416, 783, 1172, 1582, 2073, 2606]]
 
 ax = fig.add_subplot(224)
-plot_data('(c) Very Low Priority', domain4_qos)
+plot_data('(c) Very Low Priority', domain4_qos, 'Latency Percentiles', '')
 
 plt.savefig('domain2.png', bbox_inches='tight', dpi=900)
 plt.show()

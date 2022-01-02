687320243f1aa2a505d4adf3993c99b2894aafb5
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-01 22:26:25 -0700

    test commit to learn pytorch

--- plot.py
+++ plot.py
@@ -19,7 +19,7 @@ figure, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 ax1.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [293, 355, 412, 578, 742, 922, 1385, 4752], 'r', linewidth=line_width, marker='x', linestyle='dotted')
 ax1.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [143, 194, 253, 416, 570, 725, 881, 1037], 'b', linewidth=line_width, marker='d', linestyle='solid')
 ax1.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [113, 184, 202, 351, 502, 644, 791, 947], 'g', linewidth=line_width, marker='o', linestyle='-.')
-# domain2
+
 ax2.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [293, 355, 412, 578, 742, 930, 1385, 3130], 'r', linewidth=line_width, marker='x', linestyle='dotted')
 ax2.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [306, 351, 408, 570, 725, 881, 1045, 1045], 'b', linewidth=line_width, marker='d', linestyle='solid')
 ax2.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [128, 196, 273, 482, 701, 914, 1139, 1352], 'g', linewidth=line_width, marker='o', linestyle='-.')

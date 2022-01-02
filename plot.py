88149090f0e8a57b0aaa36ccfcbc81621f505402
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-01 22:25:57 -0700

    test commit to learn pytorch

--- plot.py
+++ plot.py
@@ -13,8 +13,7 @@ plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 line_width = 2
-#figure, ax1 = plt.subplots()
-#figure, ((ax1, ax2)) = plt.subplots(1, 2)
+
 figure, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 # domain1
 ax1.plot([0.5, 0.75, 0.9, 0.99, 0.999, 0.9999, 0.99999, 0.999999], [293, 355, 412, 578, 742, 922, 1385, 4752], 'r', linewidth=line_width, marker='x', linestyle='dotted')

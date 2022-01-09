4e3584302fc908c356c10aa1378415c1240c8323
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-08 21:42:30 -0700

    test commit to learn pytorch

--- line-plot-simulation-calc-2d.py
+++ line-plot-simulation-calc-2d.py
@@ -1,31 +1,31 @@
 '''
 
-Figure 10 (Multi Domain)
 x = [0.5, 0.9, 0.99, 0.999, 0.9999, 0.99999]
 
-HP Domain
-#Simulation
-y = [182, 283, 404, 513, 620, 705]
-#QoS Calculator
-y = [170, 255, 425, 510, 595, 680]
+# HP QoS
+#"Refresh Rate = 0.125"
+y = [193, 335, 507, 763, 1053, 1399]
+#"Refresh Rate = 0.25"
+y = [183, 329, 500, 753, 1024, 1252]
+#"Refresh Rate = 0.5"
+y = [165, 314, 482, 727, 997, 1185]
+#"Refresh Rate = 0.75"
+y = [149, 294, 449, 671, 908, 1207]
+#"Refresh Rate = 1"
+y = [120, 255, 398, 551, 697, 923]
 
-MP Domain
-#Simulation
-y = [199, 829, 1979, 3169, 3870, 4707]
-#QoS Calculator
-y = [170, 850, 1955, 3145, 3825, 4675]
+# VLP QoS
+#"Refresh Rate = 0.125"
+y = [179, 1243, 4184, 7948, 12061, 13801]
+#"Refresh Rate = 0.25"
+y = [171, 1023, 3369, 6135, 8657, 9914]
+#"Refresh Rate = 0.5"
+y = [155, 670, 2136, 3889, 6843, 9386]
+#"Refresh Rate = 0.75"
+y = [140, 462, 1357, 2415, 3760, 5166]
+#"Refresh Rate = 1"
+y = [111, 311, 655, 1084, 1562, 2354
 
-LP Domain
-#Simulation
-y = [186, 847, 2069, 3302, 4303, 4937]
-#QoS Calculator
-y = [170, 850, 2040, 3315, 4335, 4930]
-
-VLP Domain
-#Simulation
-y = [178, 839, 2101, 3677, 4827, 6252]
-#QoS Calculator
-y = [170, 850, 2125, 3655, 4845, 6290]
 '''
 
 
@@ -45,36 +45,35 @@ plt.rc('ytick', labelsize=size)
 plt.rc('legend', fontsize=size)
 plt.rc('figure', titlesize=size)
 
-
+# HP QoS
 x = [0.5, 0.9, 0.99, 0.999, 0.9999, 0.99999]
-# HP Domain
-# Simulation
-y11 = [182, 283, 404, 513, 620, 705]
-# QoS Calculator
-y12 = [170, 255, 425, 510, 595, 680]
+#"Refresh Rate = 0.125"
+y11 = [193, 335, 507, 763, 1053, 1399]
+#"Refresh Rate = 0.25"
+y12 = [183, 329, 500, 753, 1024, 1252]
+#"Refresh Rate = 0.5"
+y13 = [165, 314, 482, 727, 997, 1185]
+#"Refresh Rate = 0.75"
+y14 = [149, 294, 449, 671, 908, 1207]
+#"Refresh Rate = 1"
+y15 = [120, 255, 398, 551, 697, 923]
 
-# MP Domain
-# Simulation
-y21 = [199, 829, 1979, 3169, 3370, 3707]
-# QoS Calculator
-y22 = [170, 850, 1955, 3145, 3325, 3675]
-
-# LP Domain
-# Simulation
-y31 = [186, 847, 2069, 3302, 4303, 4937]
-# QoS Calculator
-y32 = [170, 850, 2040, 3315, 4335, 4930]
-
-# VLP Domain
-# Simulation
-y41 = [178, 839, 2101, 3677, 4827, 6252]
-# QoS Calculator
-y42 = [170, 850, 2125, 3655, 4845, 6290]
+# VLP QoS
+#"Refresh Rate = 0.125"
+y21 = [179, 1243, 4184, 7948, 12061, 13801]
+#"Refresh Rate = 0.25"
+y22 = [171, 1023, 3369, 6135, 8657, 9914]
+#"Refresh Rate = 0.5"
+y23 = [155, 670, 2136, 3889, 6843, 9386]
+#"Refresh Rate = 0.75"
+y24 = [140, 462, 1357, 2415, 3760, 5166]
+#"Refresh Rate = 1"
+y25 = [111, 311, 655, 1084, 1562, 2354
 
 
 fig = plt.figure(figsize=(9, 9))
 
-ax = fig.add_subplot(221)
+ax = fig.add_subplot(211)
 
 for axis in ['top', 'bottom', 'left', 'right']:
     ax.spines[axis].set_linewidth(2.5)  # change width
@@ -84,16 +83,20 @@ for axis in ['top', 'bottom', 'left', 'right']:
 plt.subplots_adjust(bottom=0.15)
 plt.plot(x, y11, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
 plt.plot(x, y12, label='QoS Calculator', color='#6C4BEE', marker='d', linestyle='dashed', linewidth=6, markersize=12)
-#plt.xlabel('Tail Latency Percentiles')
+plt.plot(x, y13, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y14, label='QoS Calculator', color='#6C4BEE', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+plt.plot(x, y15, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+
+# plt.xlabel('Tail Latency Percentiles')
 plt.ylabel('Latency (us)')
 leg = plt.legend()
-plt.ylim([0, 6500])
+plt.ylim([0, 15000])
 plt.text(.6, 2000, 'HP Domain', fontsize=25)
 leg.get_frame().set_linewidth(2.5)
 leg.get_frame().set_edgecolor("black")
 
 
-ax = fig.add_subplot(222)
+ax = fig.add_subplot(212)
 
 for axis in ['top', 'bottom', 'left', 'right']:
     ax.spines[axis].set_linewidth(2.5)  # change width
@@ -101,54 +104,16 @@ for axis in ['top', 'bottom', 'left', 'right']:
 
 
 plt.subplots_adjust(bottom=0.15)
-plt.plot(x, y21, label='Simulation', color='#0A18DA', marker='s', linestyle='solid', linewidth=6, markersize=12)
-plt.plot(x, y22, label='QoS Calculator', color='#5DDA0A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
-#plt.xlabel('Tail Latency Percentiles')
-#plt.ylabel('Latency (us)')
+plt.plot(x, y21, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y22, label='QoS Calculator', color='#6C4BEE', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+plt.plot(x, y23, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y24, label='QoS Calculator', color='#6C4BEE', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+plt.plot(x, y25, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+# plt.xlabel('Tail Latency Percentiles')
+# plt.ylabel('Latency (us)')
 leg = plt.legend()
-plt.ylim([0, 6500])
+plt.ylim([0, 15000])
 plt.text(.6, 2000, 'MP Domain', fontsize=25)
-leg.get_frame().set_linewidth(2.5)
-leg.get_frame().set_edgecolor("black")
-
-
-plt.savefig('line.png', bbox_inches='tight', dpi=600)
-
-
-ax = fig.add_subplot(223)
-
-for axis in ['top', 'bottom', 'left', 'right']:
-    ax.spines[axis].set_linewidth(2.5)  # change width
-    # ax.spines[axis].set_color('red')    # change color
-
-
-plt.subplots_adjust(bottom=0.15)
-plt.plot(x, y31, label='Simulation', color='#0A94DA', marker='s', linestyle='solid', linewidth=6, markersize=12)
-plt.plot(x, y32, label='QoS Calculator', color='#CCDA0A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
-plt.xlabel('Tail Latency Percentiles')
-plt.ylabel('Latency (us)')
-plt.ylim([0, 6500])
-leg = plt.legend()
-plt.text(.6, 2000, 'LP Domain', fontsize=25)
-leg.get_frame().set_linewidth(2.5)
-leg.get_frame().set_edgecolor("black")
-
-
-ax = fig.add_subplot(224)
-
-for axis in ['top', 'bottom', 'left', 'right']:
-    ax.spines[axis].set_linewidth(2.5)  # change width
-    # ax.spines[axis].set_color('red')    # change color
-
-
-plt.subplots_adjust(bottom=0.15)
-plt.plot(x, y41, label='Simulation', color='#DAA90A', marker='s', linestyle='solid', linewidth=6, markersize=12)
-plt.plot(x, y42, label='QoS Calculator', color='#DA250A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
-plt.xlabel('Tail Latency Percentiles')
-#plt.ylabel('Latency (us)')
-leg = plt.legend()
-plt.ylim([0, 6500])
-plt.text(.6, 2000, 'VLP Domain', fontsize=25)
 leg.get_frame().set_linewidth(2.5)
 leg.get_frame().set_edgecolor("black")
 
--- line-plot.py
+++ line-plot.py
@@ -13,8 +13,30 @@ plt.rc('ytick', labelsize=size)
 plt.rc('legend', fontsize=size)
 plt.rc('figure', titlesize=size)
 
+'''
+Figure 11 (Per-domain Bandwidth vs. Credit Refresh Rate)
+x = [0.125, 0.25, 0.5, 0.75, 1]
+HP Domain
+y = [1399314,1307639,1127945,982021,705933]
+MP Domain
+y = [920473,901974,843761,769908,568635]
+LP Domain
+y = [464291,487710,519649,501006,393771]
+VLP Domain
+y = [159203,176234,211132,223110,210375]
+'''
 
-x = np.linspace(0, 1, 20)
+# HP Domain
+y1 = [1399314, 1307639, 1127945, 982021, 705933]
+# MP Domain
+y2 = [920473, 901974, 843761, 769908, 568635]
+# LP Domain
+y3 = [464291, 487710, 519649, 501006, 393771]
+# VLP Domain
+y4 = [159203, 176234, 211132, 223110, 210375]
+
+
+x = [0.125, 0.25, 0.5, 0.75, 1]
 
 fig = plt.figure(figsize=(9, 9))
 ax = fig.add_subplot(111)
@@ -25,12 +47,13 @@ for axis in ['top', 'bottom', 'left', 'right']:
 
 
 plt.subplots_adjust(bottom=0.15)
-plt.plot(x, x * 2, label='High', color='#FF00C4', marker='s', linestyle='dashed', linewidth=2, markersize=12)
-plt.plot(x, x * 1.3, label='Medium', color='#0D61F0', marker='s', linestyle='solid', linewidth=2, markersize=12)
-plt.plot(x, x * 1.5, label='Low', color='#1B7F14', marker='s', linestyle='dotted', linewidth=2, markersize=12)
-plt.plot(x, x * 1.8, label='Very Low', color='#19CE25', marker='s', linestyle='dotted', linewidth=2, markersize=12)
+plt.plot(x, y1, label='HP', color='#FF00C4', marker='x', linestyle='dashed', linewidth=6, markersize=12)
+plt.plot(x, y2, label='MP', color='#0D61F0', marker='o', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y3, label='LP', color='#1B7F14', marker='d', linestyle='dotted', linewidth=6, markersize=12)
+plt.plot(x, y4, label='VLP', color='#19CE25', marker='s', linestyle='dotted', linewidth=6, markersize=12)
 plt.xlabel('Credit Refresh Rate')
 plt.ylabel('Achieved BW')
+plt.ylim([00000, 2000000])
 leg = plt.legend()
 leg.get_frame().set_linewidth(2.5)
 leg.get_frame().set_edgecolor("black")

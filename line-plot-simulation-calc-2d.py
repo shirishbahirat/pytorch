799aea448074ab41abc018e60e1e4336aa318acb
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-08 19:37:15 -0700

    test commit to learn pytorch

--- line-plot-simulation-calc-2d.py
+++ line-plot-simulation-calc-2d.py
@@ -46,24 +46,35 @@ plt.rc('legend', fontsize=size)
 plt.rc('figure', titlesize=size)
 
 
-x = np.linspace(.5, 1, 6)
-y1 = [89, 120, 130, 150, 190, 520]
-y2 = [85, 122, 135, 156, 193, 526]
-
-
+x = [0.5, 0.9, 0.99, 0.999, 0.9999, 0.99999]
+# HP Domain
 # Simulation
-y = [89, 171, 259, 350, 439, 521]
+y11 = [182, 283, 404, 513, 620, 705]
 # QoS Calculator
-y = [85, 170, 255, 340, 425, 510]
+y12 = [170, 255, 425, 510, 595, 680]
 
+# MP Domain
+# Simulation
+y21 = [199, 829, 1979, 3169, 3370, 3707]
+# QoS Calculator
+y22 = [170, 850, 1955, 3145, 3325, 3675]
 
-xnew = np.linspace(.5, 1, num=50, endpoint=True)
-y1new = interp1d(x, y1, kind='cubic')
-y2new = interp1d(x, y2, kind='cubic')
+# LP Domain
+# Simulation
+y31 = [186, 847, 2069, 3302, 4303, 4937]
+# QoS Calculator
+y32 = [170, 850, 2040, 3315, 4335, 4930]
+
+# VLP Domain
+# Simulation
+y41 = [178, 839, 2101, 3677, 4827, 6252]
+# QoS Calculator
+y42 = [170, 850, 2125, 3655, 4845, 6290]
+
 
 fig = plt.figure(figsize=(9, 9))
 
-ax = fig.add_subplot(211)
+ax = fig.add_subplot(221)
 
 for axis in ['top', 'bottom', 'left', 'right']:
     ax.spines[axis].set_linewidth(2.5)  # change width
@@ -71,16 +82,18 @@ for axis in ['top', 'bottom', 'left', 'right']:
 
 
 plt.subplots_adjust(bottom=0.15)
-plt.plot(xnew, y1new(xnew), label='Simulation', color='#2519CE', marker='s', linestyle='dashed', linewidth=2, markersize=12)
-plt.plot(xnew, y2new(xnew), label='QoS Calculator', color='#CE4919', marker='s', linestyle='solid', linewidth=2, markersize=12)
+plt.plot(x, y11, label='Simulation', color='#DA0A0A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y12, label='QoS Calculator', color='#6C4BEE', marker='d', linestyle='dashed', linewidth=6, markersize=12)
 #plt.xlabel('Tail Latency Percentiles')
 plt.ylabel('Latency (us)')
 leg = plt.legend()
+plt.ylim([0, 6500])
+plt.text(.6, 2000, 'HP Domain', fontsize=25)
 leg.get_frame().set_linewidth(2.5)
 leg.get_frame().set_edgecolor("black")
 
 
-ax = fig.add_subplot(212)
+ax = fig.add_subplot(222)
 
 for axis in ['top', 'bottom', 'left', 'right']:
     ax.spines[axis].set_linewidth(2.5)  # change width
@@ -88,16 +101,56 @@ for axis in ['top', 'bottom', 'left', 'right']:
 
 
 plt.subplots_adjust(bottom=0.15)
-plt.plot(xnew, y1new(xnew), label='Simulation', color='#2519CE', marker='s', linestyle='dashed', linewidth=2, markersize=12)
-plt.plot(xnew, y2new(xnew), label='QoS Calculator', color='#CE4919', marker='s', linestyle='solid', linewidth=2, markersize=12)
-plt.xlabel('Tail Latency Percentiles')
-plt.ylabel('Latency (us)')
+plt.plot(x, y21, label='Simulation', color='#0A18DA', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y22, label='QoS Calculator', color='#5DDA0A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+#plt.xlabel('Tail Latency Percentiles')
+#plt.ylabel('Latency (us)')
 leg = plt.legend()
+plt.ylim([0, 6500])
+plt.text(.6, 2000, 'MP Domain', fontsize=25)
 leg.get_frame().set_linewidth(2.5)
 leg.get_frame().set_edgecolor("black")
 
 
 plt.savefig('line.png', bbox_inches='tight', dpi=600)
+
+
+ax = fig.add_subplot(223)
+
+for axis in ['top', 'bottom', 'left', 'right']:
+    ax.spines[axis].set_linewidth(2.5)  # change width
+    # ax.spines[axis].set_color('red')    # change color
+
+
+plt.subplots_adjust(bottom=0.15)
+plt.plot(x, y31, label='Simulation', color='#0A94DA', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y32, label='QoS Calculator', color='#CCDA0A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+plt.xlabel('Tail Latency Percentiles')
+plt.ylabel('Latency (us)')
+plt.ylim([0, 6500])
+leg = plt.legend()
+plt.text(.6, 2000, 'LP Domain', fontsize=25)
+leg.get_frame().set_linewidth(2.5)
+leg.get_frame().set_edgecolor("black")
+
+
+ax = fig.add_subplot(224)
+
+for axis in ['top', 'bottom', 'left', 'right']:
+    ax.spines[axis].set_linewidth(2.5)  # change width
+    # ax.spines[axis].set_color('red')    # change color
+
+
+plt.subplots_adjust(bottom=0.15)
+plt.plot(x, y41, label='Simulation', color='#DAA90A', marker='s', linestyle='solid', linewidth=6, markersize=12)
+plt.plot(x, y42, label='QoS Calculator', color='#DA250A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
+plt.xlabel('Tail Latency Percentiles')
+#plt.ylabel('Latency (us)')
+leg = plt.legend()
+plt.ylim([0, 6500])
+plt.text(.6, 2000, 'VLP Domain', fontsize=25)
+leg.get_frame().set_linewidth(2.5)
+leg.get_frame().set_edgecolor("black")
 
 
 plt.show()

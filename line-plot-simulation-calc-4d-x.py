429b51ff81b7bab50bc1ea82d9d69a857b8a6425
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-10 21:59:32 -0700

    test commit to learn pytorch

--- line-plot-simulation-calc-4d-x.py
+++ line-plot-simulation-calc-4d-x.py
@@ -4,25 +4,6 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 
-ax = fig.add_subplot(223)
-
-for axis in ['top', 'bottom', 'left', 'right']:
-    ax.spines[axis].set_linewidth(2.5)  # change width
-    # ax.spines[axis].set_color('red')    # change color
-
-
-plt.subplots_adjust(bottom=0.15)
-plt.plot(x, y31, label='Simulation', color='#0A94DA', marker='s', linestyle='solid', linewidth=6, markersize=12)
-plt.plot(x, y32, label='QoS Calculator', color='#CCDA0A', marker='d', linestyle='dashed', linewidth=6, markersize=12)
-plt.xlabel('Tail Latency Percentiles')
-plt.ylabel('Latency (us)')
-plt.ylim([0, 6500])
-leg = plt.legend()
-plt.text(.6, 2000, 'LP Domain', fontsize=25)
-leg.get_frame().set_linewidth(2.5)
-leg.get_frame().set_edgecolor("black")
-
-
 ax = fig.add_subplot(224)
 
 for axis in ['top', 'bottom', 'left', 'right']:

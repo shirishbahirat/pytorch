9cf3898f1be2eef02ab64a876f70ae0a3323a10b
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-16 09:31:04 -0700

    test commit to learn pytorch

---
+++ tensor.py
@@ -0,0 +1,15 @@
+import torch
+
+t1 = torch.tensor(4.)
+
+t2 = torch.tensor([1., 2, 3, 4])
+
+t3 = torch.tensor([[5., 6],
+                   [7, 8],
+                   [9, 10]])
+
+t4 = torch.tensor([
+    [[11, 12, 13],
+     [13, 14, 15]],
+    [[15, 16, 17],
+     [17, 18, 19.]]])

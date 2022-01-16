7d0b714897f0b5b61bf5209c46b411cfaff9c393
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-16 15:27:20 -0700

    test commit to learn pytorch

--- lin-reg-nn.py
+++ lin-reg-nn.py
@@ -1,6 +1,10 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+
 
 inputs = np.array([[73, 67, 43],
                    [91, 88, 64],
@@ -36,45 +40,44 @@ targets = np.array([[56, 70],
                     [102, 120]],
                    dtype='float32')
 
-inputs = torch.from_numpy(inputs)
-targets = torch.from_numpy(targets)
-inputs = torch.from_numpy(inputs)
-targets = torch.from_numpy(targets)
+train_ds = TensorDataset(inputs, targets)
 
-w = torch.randn(2, 3, requires_grad=True)
-b = torch.randn(2, requires_grad=True)
+batch_size = 5
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
 
-
-def model(x):
-    return x @ w.t() + b
+model = nn.Linear(3, 2)
+loss_fn = F.mse_loss
+loss = loss_fn(model(inputs), targets)
+opt = torch.optim.SGD(model.parameters(), lr=1e-5)
 
 
-preds = model(inputs)
+def fit(num_epochs, model, loss_fn, opt, train_dl):
 
+    # Repeat for given number of epochs
+    for epoch in range(num_epochs):
 
-def mse(t1, t2):
-    diff = t1 - t2
-    return torch.sum(diff * diff) / diff.numel()
+        # Train with batches of data
+        for xb, yb in train_dl:
 
+            # 1. Generate predictions
+            pred = model(xb)
 
-loss = mse(preds, targets)
+            # 2. Calculate loss
+            loss = loss_fn(pred, yb)
 
+            # 3. Compute gradients
+            loss.backward()
 
-for epoch in range(2000):
-    preds = model(inputs)
-    loss = mse(preds, targets)
-    loss.backward()
-    with torch.no_grad():
-        w -= w.grad * 1e-5
-        b -= b.grad * 1e-5
-        w.grad.zero_()
-        b.grad.zero_()
+            # 4. Update parameters using gradients
+            opt.step()
 
-    if (epoch + 1) % 5 == 0:
-        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, 60, loss.item()))
+            # 5. Reset the gradients to zero
+            opt.zero_grad()
 
+        # Print the progress
+        if (epoch + 1) % 10 == 0:
+            print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, loss.item()))
 
-print(preds)
 
 '''
 predicted = model(torch.from_numpy(x_train)).detach().numpy()

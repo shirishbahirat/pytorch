88919937a31846ac1f7ba7165fcfbffd9ac09950
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-16 21:23:13 -0700

    test commit to learn pytorch

--- hw_obj.py
+++ hw_obj.py
@@ -4,16 +4,23 @@ class alu(object):
 
     def __init__(self):
         self.value = 0
+        self.gen = ''
 
     def step(self):
-        self.value += 1
-        yield self.value - 1
+        while (self.value < 10):
+            self.value += 1
+            yield self.value - 1
+
+    def instance(self):
+        self.gen = self.step()
 
     def next(self):
-        return next(self.step())
+        return (next(self.gen))
 
 
 a = alu()
+
+a.instance()
 
 for i in range(10):
     print(a.next())

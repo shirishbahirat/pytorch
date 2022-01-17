4bc55320e1d9ece682d4b062e33ba287321bc70b
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-16 19:50:40 -0700

    test commit to learn pytorch

--- pytrch_dl.py
+++ pytrch_dl.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 dataset = MNIST(root='data/', download=True)
@@ -63,7 +64,7 @@ for images, labels in train_loader:
     print(images.shape)
     #outputs = model(images)
     # print(outputs)
-    # break
+    break
 
 
 class MnistModel(nn.Module):
@@ -87,3 +88,112 @@ for images, labels in train_loader:
 
 print('outputs.shape : ', outputs.shape)
 print('Sample outputs :\n', outputs[:2].data)
+
+probs = F.softmax(outputs, dim=1)
+
+# Look at sample probabilities
+print("Sample probabilities:\n", probs[:2].data)
+
+# Add up the probabilities of an output row
+print("Sum: ", torch.sum(probs[0]).item())
+
+
+max_probs, preds = torch.max(probs, dim=1)
+print(preds)
+print(max_probs)
+
+
+print(labels)
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    optimizer = opt_func(model.parameters(), lr)
+    history = []  # for recording epoch-wise results
+
+    for epoch in range(epochs):
+
+        # Training Phase
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # Validation phase
+        result = evaluate(model, val_loader)
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+
+def evaluate(model, val_loader):
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+class MnistModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(input_size, num_classes)
+
+    def forward(self, xb):
+        xb = xb.reshape(-1, 784)
+        out = self.linear(xb)
+        return out
+
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss, 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
+
+
+model = MnistModel()
+
+
+result0 = evaluate(model, val_loader)
+print(result0)
+
+
+history1 = fit(5, 0.001, model, train_loader, val_loader)
+print(history1)
+
+history2 = fit(5, 0.001, model, train_loader, val_loader)
+print(history2)
+
+history3 = fit(5, 0.001, model, train_loader, val_loader)
+print(history3)
+
+history4 = fit(5, 0.001, model, train_loader, val_loader)
+print(history4)
+
+history = [result0] + history1 + history2 + history3 + history4
+accuracies = [result['val_acc'] for result in history]
+plt.plot(accuracies, '-x')
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.title('Accuracy vs. No. of epochs')
+plt.show()

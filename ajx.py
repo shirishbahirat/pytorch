3c5645e679f01e1fac88e4bace75794863d8534a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-18 20:01:33 -0700

    test commit to learn pytorch

---
+++ ajx.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision
+from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F

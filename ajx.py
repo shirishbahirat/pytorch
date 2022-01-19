1ba053c70aeda191350ed8127059239fe48e1df0
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-18 20:01:48 -0700

    test commit to learn pytorch

--- ajx.py
+++ ajx.py
@@ -7,3 +7,10 @@ from torch.utils.data import random_split
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
+
+dataset = MNIST(root='data/', download=True)
+
+print(len(dataset))
+
+test_dataset = MNIST(root='data/', train=False)
+print(len(test_dataset))

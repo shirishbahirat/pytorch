1054ca0009e9b84d5cd08a2d02e9933161ef2eb3
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-21 14:16:00 -0700

    test commit to learn pytorch

--- flaxnn.py
+++ flaxnn.py
@@ -21,3 +21,59 @@ model = MLP([12, 8, 4])
 batch = jnp.ones((32, 10))
 variables = model.init(jax.random.PRNGKey(0), batch)
 output = model.apply(variables, batch)
+
+
+class CNN(nn.Module):
+  @nn.compact
+  def __call__(self, x):
+    x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+    x = nn.relu(x)
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+    x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+    x = nn.relu(x)
+    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+    x = x.reshape((x.shape[0], -1))  # flatten
+    x = nn.Dense(features=256)(x)
+    x = nn.relu(x)
+    x = nn.Dense(features=10)(x)
+    x = nn.log_softmax(x)
+    return x
+
+
+model = CNN()
+batch = jnp.ones((32, 64, 64, 10))  # (N, H, W, C) format
+variables = model.init(jax.random.PRNGKey(0), batch)
+output = model.apply(variables, batch)
+
+
+class AutoEncoder(nn.Module):
+  encoder_widths: Sequence[int]
+  decoder_widths: Sequence[int]
+  input_shape: Sequence[int]
+
+  def setup(self):
+    input_dim = np.prod(self.input_shape)
+    self.encoder = MLP(self.encoder_widths)
+    self.decoder = MLP(self.decoder_widths + (input_dim,))
+
+  def __call__(self, x):
+    return self.decode(self.encode(x))
+
+  def encode(self, x):
+    assert x.shape[1:] == self.input_shape
+    return self.encoder(jnp.reshape(x, (x.shape[0], -1)))
+
+  def decode(self, z):
+    z = self.decoder(z)
+    x = nn.sigmoid(z)
+    x = jnp.reshape(x, (x.shape[0],) + self.input_shape)
+    return x
+
+
+model = AutoEncoder(encoder_widths=[20, 10, 5],
+                    decoder_widths=[5, 10, 20],
+                    input_shape=(12,))
+batch = jnp.ones((16, 12))
+variables = model.init(jax.random.PRNGKey(0), batch)
+encoded = model.apply(variables, batch, method=model.encode)
+decoded = model.apply(variables, encoded, method=model.decode)

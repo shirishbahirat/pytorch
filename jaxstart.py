73ec1b4f45455e32fb0c3410db6a472aeebb207c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-22 19:24:45 -0700

    test commit to learn pytorch

--- jaxstart.py
+++ jaxstart.py
@@ -25,3 +25,26 @@ print(m)
 x = jnp.dot(n, m).block_until_ready()
 
 print(x)
+
+
+a = np.random.normal(size=(4, 4))
+
+b = jnp.dot(a, m)
+
+print(b)
+
+b = jax.device_put(b)
+
+q = jnp.array([[1.0, 2.0, 3.0, 4.0],
+               [5.0, 6.0, 7.0, 8.0]])
+np.array(q)
+
+
+print(q)
+
+
+w = jnp.array([[1.0, 2.0, 3.0, 4.0],
+               [5.0, 6.0, 7.0, 8.0]])
+updated = jax.ops.index_update(w, (0, 0), 3.0)
+print("w: \n", w)
+print("updated: \n", updated)

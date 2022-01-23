b512dfff4003a95a86a6a13433bc0047a4b14b94
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-22 19:09:35 -0700

    test commit to learn pytorch

--- jaxstart.py
+++ jaxstart.py
@@ -3,7 +3,25 @@ from jax import numpy as jnp, random
 
 import numpy as np
 
+m = jnp.identity(4)
+n = jnp.array([[1.0, 2.0, 3.0, 4.0],
+               [5.0, 6.0, 7.0, 8.0]])
+
+print(n)
+print(m)
+
+x = jnp.dot(n, m).block_until_ready()
+
+print(x)
+
+
 m = jnp.ones((4, 4))
 n = jnp.array([[1.0, 2.0, 3.0, 4.0],
                [5.0, 6.0, 7.0, 8.0]])
-m
+
+print(n)
+print(m)
+
+x = jnp.dot(n, m).block_until_ready()
+
+print(x)

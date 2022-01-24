164f0081cdefae5d673a15f47e2a2a75e87c41a6
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-23 19:38:25 -0700

    test commit to learn pytorch

--- rl-learn.py
+++ rl-learn.py
@@ -73,3 +73,89 @@ baseline = 0.0
 
 # Define optimizer
 optimizer = torch.optim.RMSprop(policy_net.parameters(), lr=LEARNING_RATE)
+
+
+# Collect trajectory rewards for plotting purpose
+traj_reward_history = []
+
+# training loop
+for ep_i in range(NUM_EPISODES):
+    loss = 0.0
+
+    # Record states, actions and discounted rewards of this episode
+    states = []
+    actions = []
+    rewards = []
+    cumulative_undiscounted_reward = 0.0
+
+    for traj_i in range(BATCH_SIZE):
+        time_step = 0
+        done = False
+
+        # initialize environment
+        cur_state = env.reset()
+        cur_state = convert_to_torch_variable(cur_state)
+
+        discount_factor = 1.0
+        discounted_rewards = []
+
+        grad_log_params = []
+
+        while not done:
+            # Compute action probability using the current policy
+            action_prob = policy_net(cur_state)
+
+            # Sample action according to action probability
+            action_sampler = Bernoulli(probs=action_prob)
+            action = action_sampler.sample()
+            action = action.numpy().astype(int)[0]
+
+            # Record the states and actions -- will be used for policy gradient later
+            states.append(cur_state)
+            actions.append(action)
+
+            # take a step in the environment, and collect data
+            next_state, reward, done, _ = env.step(action)
+
+            # Discount the reward, and append to reward list
+            discounted_reward = reward * discount_factor
+            discounted_rewards.append(discounted_reward)
+            cumulative_undiscounted_reward += reward
+
+            # Prepare for taking the next step
+            cur_state = convert_to_torch_variable(next_state)
+
+            time_step += 1
+            discount_factor *= GAMMA
+
+        # Finished collecting data for the current trajectory.
+        # Recall temporal structure in policy gradient.
+        # Donstruct the "cumulative future discounted reward" at each time step.
+        for time_i in range(time_step):
+            # relevant reward is the sum of rewards from time t to the end of trajectory
+            relevant_reward = sum(discounted_rewards[time_i:])
+            rewards.append(relevant_reward)
+
+    # Finished collecting data for this batch. Update policy using policy gradient.
+    avg_traj_reward = cumulative_undiscounted_reward / BATCH_SIZE
+    traj_reward_history.append(avg_traj_reward)
+
+    if (ep_i + 1) % 10 == 0:
+        print("Episode {}: Average reward per trajectory = {}".format(ep_i + 1, avg_traj_reward))
+
+    if (ep_i + 1) % 100 == 0:
+        record_video()
+
+    optimizer.zero_grad()
+    data_len = len(states)
+    loss = 0.0
+
+    # Compute the policy gradient
+    for data_i in range(data_len):
+        action_prob = policy_net(states[data_i])
+        action_sampler = Bernoulli(probs=action_prob)
+
+        loss -= action_sampler.log_prob(actions[data_i]) * (rewards[data_i] - baseline)
+    loss /= float(data_len)
+    loss.backward()
+    optimizer.step()

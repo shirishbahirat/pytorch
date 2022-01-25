937ba08b681ef9ffa81beed850828cec87dd6f3c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-24 21:38:31 -0700

    test commit to learn pytorch

--- autograd_flatten.py
+++ autograd_flatten.py
@@ -8,3 +8,13 @@ params = [(1.0, 1.0), (2.0, 2.0), (3.0, 3.0, 3.0)]
 flat_params, unflatten_func = flatten(params)
 print('Flattened: {}'.format(flat_params))
 print('Unflattened: {}'.format(unflatten_func(flat_params)))
+
+params = [npr.randn(3, 3), npr.randn(4, 4), npr.randn(3, 3)]
+flat_params, unflatten_func = flatten(params)
+print('Flattened: {}'.format(flat_params))
+print('Unflattened: {}'.format(unflatten_func(flat_params)))
+
+params = {'weights': [1.0, 2.0, 3.0, 4.0], 'biases': [1.0, 2.0]}
+flat_params, unflatten_func = flatten(params)
+print('Flattened: {}'.format(flat_params))
+print('Unflattened: {}'.format(unflatten_func(flat_params)))

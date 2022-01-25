b79ac7e381b05827a180bec13cfab1b5fafec603
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-24 21:35:21 -0700

    test commit to learn pytorch

--- autograd_flatten.py
+++ autograd_flatten.py
@@ -2,3 +2,9 @@ import autograd.numpy as np
 import autograd.numpy.random as npr
 from autograd import grad
 from autograd.misc import flatten
+
+
+params = [(1.0, 1.0), (2.0, 2.0), (3.0, 3.0, 3.0)]
+flat_params, unflatten_func = flatten(params)
+print('Flattened: {}'.format(flat_params))
+print('Unflattened: {}'.format(unflatten_func(flat_params)))

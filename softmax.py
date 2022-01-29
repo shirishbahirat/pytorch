daf8dad4430f3bfcaca4978622d893527b1cecfa
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 14:36:45 -0700

    test commit to learn pytorch

--- softmax.py
+++ softmax.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def softmax(x):
@@ -8,3 +9,6 @@ def softmax(x):
 x = np.array([2., 1., .1])
 
 print(softmax(x))
+
+x = torch.tensor(x)
+print(torch.softmax(x, dim=0))

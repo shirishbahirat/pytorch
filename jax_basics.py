87874aac3fbff4e84a64ed6dd8628d950ff723a2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 10:30:04 -0700

    test commit to learn pytorch

--- jax_basics.py
+++ jax_basics.py
@@ -7,3 +7,5 @@ m = jnp.ones((4, 4))  # We're generating one 4 by 4 matrix filled with ones.
 n = jnp.array([[1.0, 2.0, 3.0, 4.0],
                [5.0, 6.0, 7.0, 8.0]])  # An explicit 2 by 4 array
 print(m)
+
+jnp.dot(n, m).block_until_ready()

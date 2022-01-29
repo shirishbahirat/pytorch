98fd5680d940f95569cf71a5decb4e40ae051f16
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 10:29:24 -0700

    test commit to learn pytorch

--- jax_basics.py
+++ jax_basics.py
@@ -2,3 +2,8 @@ import jax
 from jax import numpy as jnp, random
 
 import numpy as np
+
+m = jnp.ones((4, 4))  # We're generating one 4 by 4 matrix filled with ones.
+n = jnp.array([[1.0, 2.0, 3.0, 4.0],
+               [5.0, 6.0, 7.0, 8.0]])  # An explicit 2 by 4 array
+print(m)

f9693f3745f7c3109cf71f3899fbcac35275af75
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 20:41:22 -0700

    test commit to learn pytorch

--- gra.py
+++ gra.py
@@ -17,3 +17,25 @@ k = torch.tensor(10., requires_grad=True)
 m = k + 10
 m.backward()
 print(k.grad)
+
+
+'''
+x = a(x)
+x = b(y)
+dx/dx = dz/dy * dy/dx
+
+'''
+
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0, requires_grad=True)
+
+y_hat = w * x
+loss = (y_hat - y)**2
+
+print(loss)
+
+loss.backward()
+print(w.grad)

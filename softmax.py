84e65183a37458ff1df1922eb4c6b2d1cf7a848e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 17:52:39 -0700

    test commit to learn pytorch

--- softmax.py
+++ softmax.py
@@ -48,3 +48,23 @@ _, predictions2 = torch.max(Y_pred_bad, 1)
 
 print(predictions1)
 print(predictions2)
+
+
+Y = torch.tensor([2, 0, 1])
+Y_pred_good = torch.tensor([[0.7, 0.2, 0.1], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]])
+Y_pred_bad = torch.tensor([[0.1, 0.3, 0.6], [0.7, 0.2, 0.1], [0.7, 0.2, 0.1]])
+
+l1 = loss(Y_pred_good, Y)
+l2 = loss(Y_pred_bad, Y)
+
+print(f'loss1 numpy: {l1.item():.4f}')
+print(f'loss2 numpy: {l2.item():.4f}')
+
+'''
+_, predictions1 = torch.max(Y_pred_good, 1)
+_, predictions2 = torch.max(Y_pred_bad, 1)
+
+
+print(predictions1)
+print(predictions2)
+'''

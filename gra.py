3b9575fa90d51fa59bfb501571b720c5e4152ea0
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-29 20:16:30 -0700

    test commit to learn pytorch

--- gra.py
+++ gra.py
@@ -3,7 +3,12 @@ import torch
 x = torch.tensor([.1, .2, .3], requires_grad=True)
 
 y = x**2 + 2
+print(y)
 v = torch.tensor([0.1, 0.01, 0.01], dtype=torch.float32)
 y.backward(x)
 
 print(x.grad)
+
+x = torch.randn(3, requires_grad=True)
+y = x + 2
+y.backward(x)

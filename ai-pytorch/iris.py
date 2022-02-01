ed0308af40db837c0d6b3bb7e3630fa26caad604
Shirish Bahirat <shirishbahirat@comcast.net>
2022-01-31 20:41:59 -0700

    test commit to learn pytorch

--- ai-pytorch/iris.py
+++ ai-pytorch/iris.py
@@ -45,3 +45,89 @@ ax2.set_xlabel(feature_names[2])
 ax2.set_ylabel(feature_names[3])
 ax2.axis('equal')
 ax2.legend()
+# plt.show()
+
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class Model(nn.Module):
+    def __init__(self, input_dim):
+        super(Model, self).__init__()
+        self.layer1 = nn.Linear(input_dim, 50)
+        self.layer2 = nn.Linear(50, 50)
+        self.layer3 = nn.Linear(50, 3)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = F.softmax(self.layer3(x), dim=1)
+        return x
+
+
+model = Model(X_train.shape[1])
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.CrossEntropyLoss()
+
+
+import tqdm
+
+EPOCHS = 100
+X_train = Variable(torch.from_numpy(X_train)).float()
+y_train = Variable(torch.from_numpy(y_train)).long()
+X_test = Variable(torch.from_numpy(X_test)).float()
+y_test = Variable(torch.from_numpy(y_test)).long()
+
+loss_list = np.zeros((EPOCHS,))
+accuracy_list = np.zeros((EPOCHS,))
+
+for epoch in tqdm.trange(EPOCHS):
+    y_pred = model(X_train)
+    loss = loss_fn(y_pred, y_train)
+    loss_list[epoch] = loss.item()
+
+    # Zero gradients
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    with torch.no_grad():
+        y_pred = model(X_test)
+        correct = (torch.argmax(y_pred, dim=1) == y_test).type(torch.FloatTensor)
+        accuracy_list[epoch] = correct.mean()
+
+
+print(model)
+
+fig, (ax1, ax2) = plt.subplots(2, figsize=(12, 6), sharex=True)
+
+ax1.plot(accuracy_list)
+ax1.set_ylabel("validation accuracy")
+ax2.plot(loss_list)
+ax2.set_ylabel("validation loss")
+ax2.set_xlabel("epochs")
+
+
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import OneHotEncoder
+
+plt.figure(figsize=(10, 10))
+plt.plot([0, 1], [0, 1], 'k--')
+
+# One hot encoding
+enc = OneHotEncoder()
+Y_onehot = enc.fit_transform(y_test[:, np.newaxis]).toarray()
+
+with torch.no_grad():
+    y_pred = model(X_test).numpy()
+    fpr, tpr, threshold = roc_curve(Y_onehot.ravel(), y_pred.ravel())
+
+plt.plot(fpr, tpr, label='AUC = {:.3f}'.format(auc(fpr, tpr)))
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve')
+plt.legend()
+
+plt.show()

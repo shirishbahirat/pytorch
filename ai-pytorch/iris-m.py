0b4cc7a4fa89fa586ff190a11e4a03a2f80ce577
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-02 21:02:29 -0700

    test commit to learn pytorch

--- ai-pytorch/iris-m.py
+++ ai-pytorch/iris-m.py
@@ -18,6 +18,7 @@ feature_names = iris['feature_names']
 Scale data to have mean 0 and variance 1 which is
 importance for convergence of the neural network
 '''
+
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 

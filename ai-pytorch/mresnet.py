f69492409d3077fe8e7f4f0ad61d195dd3943432
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-03 20:29:34 -0700

    test commit to learn pytorch

--- ai-pytorch/mresnet.py
+++ ai-pytorch/mresnet.py
@@ -6,3 +6,10 @@ class block(nn.Module):
     def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
         super(block, self).__init__()
         self.expansion = 4
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(out_channels * self.expansion)
+        self.relu = nn.ReLU()

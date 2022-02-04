3af92a9d288a508a7ed2dce20039224f12192e43
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-03 19:48:20 -0700

    test commit to learn pytorch

--- ai-pytorch/pteinsum.py
+++ ai-pytorch/pteinsum.py
@@ -6,3 +6,15 @@ x = torch.rand((2, 3))
 torch.einsum('ij->ji', x)
 
 print(x, torch.einsum('ij->ji', x))
+
+# summation
+
+print(torch.einsum('ij->', x))
+
+# column sum of
+
+print(torch.einsum('ij->', x))
+
+# row sum
+
+print(torch.einsum('ij->', x))

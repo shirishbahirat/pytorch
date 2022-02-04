0a48d528d07a6160f555a3e9f6dfc74abaf820e7
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-03 20:24:07 -0700

    test commit to learn pytorch

---
+++ ai-pytorch/mresnet.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+
+
+class block(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(block, self).__init__()

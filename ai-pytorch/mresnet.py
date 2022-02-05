cfddf4faaa33bafa7fb12943598a40823f470b50
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-05 08:32:56 -0700

    test commit to learn pytorch

--- ai-pytorch/mresnet.py
+++ ai-pytorch/mresnet.py
@@ -66,6 +66,8 @@ class ResNet(nn.Module):
 
         x = self.avgpool(x)
         x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        return x
 
     def make_layer(self, block, num_residual_blocks, out_channels, strides):
         identity_downsample = None

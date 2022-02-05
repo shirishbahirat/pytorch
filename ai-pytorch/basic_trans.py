67169eded3148ba36fba1c9991ba606850c6ee59
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-05 11:34:13 -0700

    test commit to learn pytorch

--- ai-pytorch/basic_trans.py
+++ ai-pytorch/basic_trans.py
@@ -1,2 +1,17 @@
 import torch
 import torch.nn.functional as F
+
+# assume we have some tensor x with size (b, t, k)
+x = torch.rand(2, 3, 4)
+
+print(x)
+
+raw_weights = torch.bmm(x, x.transpose(1, 2))
+# - torch.bmm is a batched matrix multiplication. It
+#   applies matrix multiplication over batches of
+#   matrices.
+
+
+print(x.transpose())
+
+print(raw_weights)

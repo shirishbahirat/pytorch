9aa19be3e92d317dac580fe3c91ff200e2f3dcd2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-07 11:46:26 -0700

    test commit to learn pytorch

---
+++ hackthon.py
@@ -0,0 +1,13 @@
+
+# calculate the spearmans's correlation between two variables
+from numpy.random import randn
+from numpy.random import seed
+from scipy.stats import spearmanr
+# seed random number generator
+seed(1)
+# prepare data
+data1 = 20 * randn(1000) + 100
+data2 = data1 + (10 * randn(1000) + 50)
+# calculate spearman's correlation
+corr, _ = spearmanr(data1, data2)
+print('Spearmans correlation: %.3f' % corr)

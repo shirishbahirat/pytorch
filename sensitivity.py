c4f3746b3d3f499540f35bbb10a5a3ccf9e6368b
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-07 12:32:20 -0700

    test commit to learn pytorch

---
+++ sensitivity.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+
+# cast to float Tensor
+X = torch.from_numpy(X_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
+y = y.view(y.shape[0], 1)
+
+n_samples, n_features = X.shape

903483e5f6ad86aa1380444dd50f433e8a9752ff
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-10 08:55:12 -0700

    test commit to learn pytorch

--- policy_grad.py
+++ policy_grad.py
@@ -1,14 +1,18 @@
 observations = [
     [2, 1],
     [1.0],
+    [2, 0],
+    [2, 1],
+    [1, 0], [2, 1], [2, 1], [1, 0], [2, 0],
+
 ]
 
 # action 0 left, 1 right,
 
-actions = [0, 1]
+actions = [0, 1, 0, 0, 0, 1, 0, 1, 1]
 
-rewards = [3, 0]
+rewards = [3, 0, 0, 3, -10, 10, 3, 0, 10]
 
-future_returns = []
+future_returns = [3, 0, 0, -7, -10, 10, 13, 10, 10]  # update once episode done
 
-num_episodes = 0
+num_episodes = 4

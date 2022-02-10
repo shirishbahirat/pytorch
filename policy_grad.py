978aa2c8a1243f08d44d56050f60bd8c2903ba15
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-10 08:59:27 -0700

    test commit to learn pytorch

--- policy_grad.py
+++ policy_grad.py
@@ -16,3 +16,14 @@ rewards = [3, 0, 0, 3, -10, 10, 3, 0, 10]
 future_returns = [3, 0, 0, -7, -10, 10, 13, 10, 10]  # update once episode done
 
 num_episodes = 4
+
+net = nn.Linear(2, 2)
+logits = net(observations)
+
+policy_distributions = distributions.Categorical(logits=logits)
+
+log_probs = policy_distributions.log_probs(actions)
+
+loss = -(log_probs * future_returns).sum() / num_episodes
+
+loss.backward()

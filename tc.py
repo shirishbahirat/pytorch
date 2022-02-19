c7c3388a1345dc40c77b236bd5db93cdc22d044e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-19 14:52:16 -0700

    test commit to learn pytorch

---
+++ tc.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.autograd
+from torch.autograd import Variable
+
+X = torch.tensor([[i] for i in range(10)])
+Y = X*2
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.linear = nn.Linear(1,1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+model = Model()
+
+print(model)
+
+for epoch in range(100):
+    

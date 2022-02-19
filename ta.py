3a6fd88beb290d1d2dbf5f834e5db8ad7c3f8a84
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-19 10:05:04 -0700

    test commit to learn pytorch

--- ta.py
+++ ta.py
@@ -2,3 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
+
+x = Variable(torch.tensor([[1.],[2.],[3.],[4.],[5.]]))
+x = Variable(torch.tensor([[1.],[2.],[3.],[4.],[5.]]))

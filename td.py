09f169b6d7942f61ffefdce8e8294e991b306c29
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-20 09:01:00 -0700

    test commit to learn pytorch

---
+++ td.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from random import random
+
+x = torch.tensor([[float(i)*.01] for i in range(10)])
+y = x*-2. + 3 + random()
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.l1 = nn.Linear(1,1)
+
+    def forward(self,x):
+        return self.l1(x)
+
+model = Model()
+
+criteria = nn.MSELoss(reduction='sum')
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+
+
+for epoch in range(1000):
+    y_pred = model(x)
+    loss = criteria(y_pred, x)
+
+    print('Epoch {:3.0f} loss {:4.4f}'.format(epoch,
+    loss.item()))
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+

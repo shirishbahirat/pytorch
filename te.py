df46b79bc0a1d3eb3364e32c359f61cfa8cbd8a2
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-20 09:52:16 -0700

    test commit to learn pytorch

--- te.py
+++ te.py
@@ -1,3 +1,4 @@
+from statistics import mode
 import torch
 import torch.nn as nn
 from random import random 
@@ -19,3 +20,19 @@ model = Model()
 criterion = nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
+for epoch in range(500):
+
+    y_pred = model(x)
+    loss = criterion(y_pred, y)
+
+    print('Epoch {:4.0f} | Loss {:4.4f}'.format(epoch, loss.item()))
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+p = .1
+x_data = torch.tensor([[p]])
+y_pred = model(x_data)
+
+print('Predicted {} : {}'.format(p, y_pred[0][0].item()))

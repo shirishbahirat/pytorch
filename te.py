cd1b49a38974d345767e8b099f89eade493e35a7
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-20 09:42:54 -0700

    test commit to learn pytorch

--- te.py
+++ te.py
@@ -5,3 +5,11 @@ from random import random
 x = torch.tensor([[float(i)*.1] for i in range(10)])
 y = x*-3.0 + 3.3 + random()
 
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.liner = nn.Linear(1,1)
+
+    def forward(self, x):
+        y_pred = self.liner(x)
+        return y_pred

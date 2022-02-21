3ff9f20087daf48373368ed9c34890d4328d3cfa
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 15:06:17 -0700

    test commit to learn pytorch

--- tm.py
+++ tm.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from random import random 
-import torch._tensor as tensor
 
-x = tensor([[i*0.1] for i in range(10)])
+x = torch.tensor([[i*0.1] for i in range(10)])
+y = torch.tensor([[i*-3.3 + 10 + random()] for i in range(10)])
+
+print(x.T,y.T)
+

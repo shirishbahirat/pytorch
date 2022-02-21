eaea029f66a750c734def393ca2b44c5809a995e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-20 20:22:26 -0700

    test commit to learn pytorch

---
+++ tg.py
@@ -0,0 +1,20 @@
+from statistics import mode
+import torch
+import torch.nn as nn
+from random import random
+
+x = torch.tensor([[float(i)*.1] for i in range(10)])
+y = x*2. + 2. + random()
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super(Model,self).__init__()
+        self.linear = nn.Linear(1,1)
+
+    def forward(self,x):
+        y_pred = nn.Linear(x)
+        return y_pred
+    
+model = Model()
+print(model)

731e393bf0c4428c0a8ab53ab4fd46c049128bba
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 15:41:16 -0700

    test commit to learn pytorch

--- tm.py
+++ tm.py
@@ -8,3 +8,12 @@ y = torch.tensor([[i*-3.3 + 10 + random()] for i in range(10)])
 
 print(x.T,y.T)
 
+class Model(nn.Module):
+    def __init__(self):
+        super(Model,self).__init__()
+        self.linear = nn.Linear(1,1)
+
+    def forward(self,x):
+        y_pred = self.linear(x)
+        return y_pred
+

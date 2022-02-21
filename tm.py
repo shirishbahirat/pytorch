bbfbc32a99d7f3ed4abdbfaeee809e8a2ff86344
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 15:00:01 -0700

    test commit to learn pytorch

--- tm.py
+++ tm.py
@@ -2,6 +2,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from random import random 
-import torch.tensor as tensor
+import torch._tensor as tensor
 
-x = tensor()
+x = tensor([[i*0.1] for i in range(10)])

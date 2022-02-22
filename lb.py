020298eea73212e410b81c20fd579b42099566fb
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 19:36:34 -0700

    test commit to learn pytorch

--- lb.py
+++ lb.py
@@ -30,6 +30,40 @@ net = nn.Sequential(
     nn.ELU(),
     nn.Linear(n_hidden, n_output),
     nn.Sigmoid())
-    
+
 print(net)
+
+loss_func = nn.BCELoss()
+learning_rate = 0.01
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+
+train_loss = []
+train_accuracy = []
+iters = 1000
+Y_train_t = torch.FloatTensor(Y_train).reshape(-1, 1)
+for i in range(iters):
+    X_train_t = torch.FloatTensor(X_train)
+    y_hat = net(X_train_t)
+    loss = loss_func(y_hat, Y_train_t)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    y_hat_class = np.where(y_hat.detach().numpy()<0.5, 0, 1)
+    accuracy = np.sum(Y_train.reshape(-1,1)==y_hat_class) / len(Y_train)
+    train_accuracy.append(accuracy)
+    train_loss.append(loss.item())
+    
+fig, ax = plt.subplots(2, 1, figsize=(12,8))
+ax[0].plot(train_loss)
+ax[0].set_ylabel('Loss')
+ax[0].set_title('Training Loss')
+
+ax[1].plot(train_accuracy)
+ax[1].set_ylabel('Classification Accuracy')
+ax[1].set_title('Training Accuracy')
+
+plt.tight_layout()
+plt.show()
+
+
 

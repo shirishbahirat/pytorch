c11ec4b6eddf0bf8ab0d8e649fa1fd3e2900862d
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 19:31:49 -0700

    test commit to learn pytorch

--- lb.py
+++ lb.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+
+np.random.seed(0)
+torch.manual_seed(0)
+X, Y = make_moons(500, noise=0.2)
+
+# Split into test and training data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+    test_size=0.25, random_state=73)

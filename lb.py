a760de2095c65351dea4580c933cf3a446dcf038
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 19:30:13 -0700

    test commit to learn pytorch

--- lb.py
+++ lb.py
@@ -1,2 +1,6 @@
-import sklearn.datasets
-X,y = sklearn.datasets.make_moons(200,noise=0.2) 
+import numpy as np
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn

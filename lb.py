cf12ec72e518479112ecdc19c6948c341ca965cd
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-21 19:33:08 -0700

    test commit to learn pytorch

--- lb.py
+++ lb.py
@@ -12,3 +12,24 @@ X, Y = make_moons(500, noise=0.2)
 # Split into test and training data
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
     test_size=0.25, random_state=73)
+
+plt.figure(figsize=(8,6))
+plt.scatter(X_train[:,0], X_train[:,1], c=Y_train)
+plt.title('Moon Data')
+plt.show()
+
+# Define network dimensions
+n_input_dim = X_train.shape[1]
+# Layer size
+n_hidden = 4 # Number of hidden nodes
+n_output = 1 # Number of output nodes = for binary classifier
+
+# Build your network
+net = nn.Sequential(
+    nn.Linear(n_input_dim, n_hidden),
+    nn.ELU(),
+    nn.Linear(n_hidden, n_output),
+    nn.Sigmoid())
+    
+print(net)
+

bfbc375349d6450746209b559133026af3a784fc
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-25 06:27:14 -0700

    test commit to learn pytorch

--- loss.py
+++ loss.py
@@ -1,9 +1,12 @@
+from black import out
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
 output = Variable(torch.FloatTensor([0,0,0,1])).view(1, -1)
 target = Variable(torch.LongTensor([3]))
+
+print(output)
 
 criterion = nn.CrossEntropyLoss()
 loss = criterion(output, target)

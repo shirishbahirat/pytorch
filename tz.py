1da7706431c310902dfa94946ba227078596409c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-02-26 12:02:31 -0700

    test commit to learn pytorch

--- tz.py
+++ tz.py
@@ -22,7 +22,7 @@ class Model(nn.Module):
 
 model = Model()
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
 
 train_loss = []
 

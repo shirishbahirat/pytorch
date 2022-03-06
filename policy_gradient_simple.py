52905e5f4c7e411b37b5e8bb8ccdb0df4f2c6a8a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-05 19:47:14 -0700

    test commit to learn pytorch

--- policy_gradient_simple.py
+++ policy_gradient_simple.py
@@ -33,3 +33,22 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 #A Input data is length 4
 #B Output is a 2-length vector for the Left and the Right actions
 #C Output is a softmax probability distribution over actions
+
+state1 = env.reset()
+pred = model(torch.from_numpy(state1).float()) #G
+action = np.random.choice(np.array([0,1]), p=pred.data.numpy()) #H
+state2, reward, done, info = env.step(action) #I
+
+#G Call policy network model to produce predicted action probabilities
+#H Sample an action from the probability distribution produced by the policy network
+#I Take the action, receive new state and reward. The info variable is produced by the environment but is irrelevant
+
+def discount_rewards(rewards, gamma=0.99):
+    lenr = len(rewards)
+    disc_return = torch.pow(gamma,torch.arange(lenr).float()) * rewards #A
+    disc_return /= disc_return.max() #B
+    return disc_return
+
+#A Compute exponentially decaying rewards
+#B Normalize the rewards to be within the [0,1] interval to improve numerical stability
+

82ce036081467284da2ac704e62308775fb60762
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-05 19:46:06 -0700

    test commit to learn pytorch

--- policy_gradient_simple.py
+++ policy_gradient_simple.py
@@ -11,3 +11,25 @@ def running_mean(x, N=50):
         y[i] = kernel @ x[i:i+N]
         y[i] /= N
     return y
+
+import gym
+import numpy as np
+import torch
+
+l1 = 4 #A
+l2 = 150
+l3 = 2 #B
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(l1, l2),
+    torch.nn.LeakyReLU(),
+    torch.nn.Linear(l2, l3),
+    torch.nn.Softmax(dim=0) #C
+)
+
+learning_rate = 0.009
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+#A Input data is length 4
+#B Output is a 2-length vector for the Left and the Right actions
+#C Output is a softmax probability distribution over actions

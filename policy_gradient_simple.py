c476b6098e0b72bc1dd03e1ae4dfbdf6304c1388
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-05 19:48:05 -0700

    test commit to learn pytorch

--- policy_gradient_simple.py
+++ policy_gradient_simple.py
@@ -52,3 +52,58 @@ def discount_rewards(rewards, gamma=0.99):
 #A Compute exponentially decaying rewards
 #B Normalize the rewards to be within the [0,1] interval to improve numerical stability
 
+def loss_fn(preds, r): #A
+    return -1 * torch.sum(r * torch.log(preds)) #B
+
+#A The loss function expects an array of action probabilities for the actions that were taken and the discounted rewards.
+#B It computes the log of the probabilities, multiplies by the discounted rewards, sums them all and flips the sign.
+
+MAX_DUR = 200
+MAX_EPISODES = 500
+gamma = 0.99
+score = [] #A
+expectation = 0.0
+for episode in range(MAX_EPISODES):
+    curr_state = env.reset()
+    done = False
+    transitions = [] #B
+    
+    for t in range(MAX_DUR): #C
+        act_prob = model(torch.from_numpy(curr_state).float()) #D
+        action = np.random.choice(np.array([0,1]), p=act_prob.data.numpy()) #E
+        prev_state = curr_state
+        curr_state, _, done, info = env.step(action) #F
+        transitions.append((prev_state, action, t+1)) #G
+        if done: #H
+            break
+
+    ep_len = len(transitions) #I
+    score.append(ep_len)
+    reward_batch = torch.Tensor([r for (s,a,r) in transitions]).flip(dims=(0,)) #J
+    disc_returns = discount_rewards(reward_batch) #K
+    state_batch = torch.Tensor([s for (s,a,r) in transitions]) #L
+    action_batch = torch.Tensor([a for (s,a,r) in transitions]) #M
+    pred_batch = model(state_batch) #N
+    prob_batch = pred_batch.gather(dim=1,index=action_batch.long().view(-1,1)).squeeze() #O
+    loss = loss_fn(prob_batch, disc_returns)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+#A List to keep track of the episode length over training time
+#B List of state, action, rewards (but we ignore the reward)
+#C While in episode
+#D Get the action probabilities
+#E Select an action stochastically
+#F Take the action in the environment
+#G Store this transition
+#H If game is lost, break out of the loop
+#I Store the episode length
+#J Collect all the rewards in the episode in a single tensor
+#K Compute the discounted version of the rewards
+#L Collect the states in the episode in a single tensor
+#M Collect the actions in the episode in a single tensor
+#N Re-compute the action probabilities for all the states in the episode
+#O Subset the action-probabilities associated with the actions that were actually taken 
+
+

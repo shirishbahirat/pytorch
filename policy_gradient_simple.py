f1f9a6ad6d5e24bd69b0940e5245a0309806c00e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-05 19:44:15 -0700

    test commit to learn pytorch

--- policy_gradient_simple.py
+++ policy_gradient_simple.py
@@ -2,3 +2,12 @@ import numpy as np
 import torch
 import gym
 from matplotlib import pyplot as plt
+
+def running_mean(x, N=50):
+    kernel = np.ones(N)
+    conv_len = x.shape[0]-N
+    y = np.zeros(conv_len)
+    for i in range(conv_len):
+        y[i] = kernel @ x[i:i+N]
+        y[i] /= N
+    return y

8ac485d1701b30509e10575f4a32519fb2bc484e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-12 14:12:27 -0700

    test commit to learn pytorch

--- centrophy.py
+++ centrophy.py
@@ -2,8 +2,10 @@ import torch
 import torch.nn as nn
 
 
-# Example of target with class probabilities
 input = torch.randn(3, 5, requires_grad=True)
 target = torch.randn(3, 5).softmax(dim=1)
 output = loss(input, target)
 output.backward()
+
+
+

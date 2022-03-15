c96fc3caf51482da9538cad42dbaa4687bda9243
Shirish Bahirat <shirishbahirat@comcast.net>
2022-03-14 20:44:00 -0600

    test commit to learn pytorch

--- bk.py
+++ bk.py
@@ -8,7 +8,6 @@ target = torch.empty(3, dtype=torch.long).random_(5)
 output = loss(input, target)
 output.backward()
 
-
 # Example of target with class probabilities
 input = torch.randn(3, 5, requires_grad=True)
 target = torch.randn(3, 5).softmax(dim=1)

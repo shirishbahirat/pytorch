798379ee7c27c1671e50b4cbd628a8a35146ba50
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-24 17:57:08 -0600

    test commit to learn pytorch

--- to.py
+++ to.py
@@ -19,9 +19,9 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.linear1 = nn.Linear(19,32)
-        self.linear2 = nn.Linear(32,64)
-        self.linear3 = nn.Linear(64,19)
-        self.linear4 = nn.Linear(19,19)
+        self.linear2 = nn.Linear(32,128)
+        self.linear3 = nn.Linear(128,64)
+        self.linear4 = nn.Linear(64,19)
 
     def forward(self,x):
         y_hat = F.relu(self.linear1(x))

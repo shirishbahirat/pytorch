c4845224a0910250880b0e6863ad1e5547ae029a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-24 09:25:22 -0600

    test commit to learn pytorch

--- to.py
+++ to.py
@@ -2,20 +2,22 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import numpy as np
 
-a = [i*i for i in range(-9,10)]
-b = [i*i+5 for i in range(-9,10)]
+a = np.array([float(i*i) for i in range(-9,10)])
+b = np.array([float(i*i+5.0) for i in range(-9,10)])
 
-input = torch.tensor(a, dtype=float, requires_grad=True)
-target = torch.tensor(b, dtype=float, requires_grad=True)
+
+input = torch.tensor(a, dtype=torch.float32, requires_grad=True)
+target = torch.tensor(b, dtype=torch.float32)
 
 class Model(nn.Module):
 
     def __init__(self):
         super(Model, self).__init__()
-        self.linear1 = nn.Linear(19,10)
-        self.linear2 = nn.Linear(10,19)
-        self.linear3 = nn.Linear(8,19)
+        self.linear1 = nn.Linear(19,6)
+        self.linear2 = nn.Linear(6,19)
+        self.linear3 = nn.Linear(19,1)
 
     def forward(self,x):
         y_hat = F.relu(self.linear1(x))
@@ -29,7 +31,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
 train_loss = []
 
-for epoch in range(1000):
+for epoch in range(100):
 
     y_hat = model(input)
     optimizer.zero_grad()

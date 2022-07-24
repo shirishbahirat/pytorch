31586448d7abdf426180869c2b663b1f301e3ba1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-24 17:55:48 -0600

    test commit to learn pytorch

--- to.py
+++ to.py
@@ -7,8 +7,11 @@ import numpy as np
 a = np.array([(i*i) for i in range(-9,10)])
 b = np.array([(i*i+5.0) for i in range(-9,10)])
 
-input = torch.tensor(a/max(a), dtype=torch.float32, requires_grad=True)
-target = torch.tensor(b/max(b), dtype=torch.float32)
+a = a/max(a)
+b = b/max(b)
+
+input = torch.tensor(a, dtype=torch.float32, requires_grad=True)
+target = torch.tensor(b, dtype=torch.float32)
 
 
 class Model(nn.Module):

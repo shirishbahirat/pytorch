21089d7b1c72cb7ec23e291eeb53a4e514bfb448
Shirish Bahirat <shirishbahirat@comcast.net>
2022-07-24 18:21:23 -0600

    test commit to learn pytorch

--- to.py
+++ to.py
@@ -56,7 +56,7 @@ out= model(input)
 print(out, a, b)
 
 plt.plot(out.detach().numpy())
-plt.plot(target.detach().numpy()+.2)
+plt.plot(target.detach().numpy())
 plt.plot(input.detach().numpy())
 plt.show()
 

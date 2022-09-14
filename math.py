d8b137df0bc464d0bfa43500d4b735790e293a80
Shirish Bahirat <shirishbahirat@comcast.net>
2022-09-14 16:58:57 -0600

    test commit to learn pytorch

--- math.py
+++ math.py
@@ -4,5 +4,11 @@ class mk:
    def __init__(self,val):
      self.val = val
    def __add__(self,other):
-     out = mk(self.data + other.data, (self, other), '+')
+     out = mk(self.val + other.val, (self, other), '+')
      return out
+
+
+a = mk(3)
+b = mk(4)
+
+c = a + b

4de936fc5ae1137e15226974a09c24c157b3157c
Shirish Bahirat <shirishbahirat@comcast.net>
2022-09-17 10:00:30 -0600

    test commit to learn pytorch

--- mgrad.py
+++ mgrad.py
@@ -6,6 +6,7 @@ def f(x):
   return 3*x**2 - 4*x + 5
 
 #6*x - 4
+#6*(2/3) - 4 = 0
 
 print(f(3.0))
 
@@ -15,4 +16,8 @@ ys = f(xs)
 
 h = 0.000001
 x = 3
+print((f(x + h) - f(x))/h)
+
+h = 0.000001
+x = 2/3
 print((f(x + h) - f(x))/h)

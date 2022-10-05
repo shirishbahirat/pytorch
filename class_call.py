9401ece21bd6c7e4636e06fe9ad106fa57d19bb6
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-04 19:26:32 -0600

    test commit to learn pytorch

--- class_call.py
+++ class_call.py
@@ -5,9 +5,11 @@ class model:
 	def __init__(self, data):
 		self.data = data
 
-	def __call__(self):
+	def __call__(self, y):
 		print(self.data)
 		print('from class call')
+		for i in self.data:
+			print(i + y)
 
 	def __repr__(self):
 		print(f'model:')
@@ -17,7 +19,7 @@ class model:
 def main():
 
 	m = model([1,2,3])
-	m()
+	m(9)
 
 
 if __name__ == '__main__':

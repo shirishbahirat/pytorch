72cfb8f5b44e577041c5b83b84b2ea4fbc82ebf4
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-12 21:13:34 -0600

    test commit to learn pytorch

--- grd.py
+++ grd.py
@@ -16,8 +16,8 @@ def gradn(w1, w2, x1, x2, b):
     return w1*x1 + w1*x2 + w2*x1 + w2*x2 + b
 
 
-hm = gradn(2,3,1,4,-3)
-hd = gradn(2,3+h,1,4,-3)
+hm = gradn(4,3,10,7,-3)
+hd = gradn(4,3+h,10,7,-3)
 
 xc = (hd - hm)/h
 

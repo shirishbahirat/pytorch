fbe238db78c121e54b09a2a02f03312e3f4fd92e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-12 21:10:14 -0600

    test commit to learn pytorch

--- grd.py
+++ grd.py
@@ -1,6 +1,6 @@
 
 def grad(a, b, c):
-	return a*b + c
+    return a*b + c
 
 h = 0.0001
 
@@ -10,3 +10,8 @@ fh = grad(2,3,-2+h)
 g = (fh - f)/h
 
 print(g)
+
+
+def grad(w1, w2, x1, x2, b):
+    return w1*x1 + w1*x2 + w2*x1 + w2*x2 + b
+

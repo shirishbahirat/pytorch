77f9a62e75a4b19181ed58c2b7bd9949ba1dd3cf
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-20 20:12:15 -0600

    test commit to learn pytorch

--- tg-model.py
+++ tg-model.py
@@ -4,3 +4,15 @@ import matplotlib.pyplot as plt
 
 from tinygrad import Value
 from nn import Neuron, Layer, MLP
+
+np.random.seed(1337)
+random.seed(1337)
+
+from sklearn.datasets import make_moons, make_blobs
+X, y = make_moons(n_samples=100, noise=0.1)
+
+y = y*2 - 1 # make y be -1 or 1
+# visualize in 2D
+plt.figure(figsize=(5,5))
+plt.scatter(X[:,0], X[:,1], c=y, s=20, cmap='jet')
+plt.show()

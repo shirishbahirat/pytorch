7de40fb3c0032d3fa584cb7aed1c31c552356d2f
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-22 11:11:11 -0700

    test commit to learn pytorch

--- asyn.py
+++ asyn.py
@@ -1,9 +1,11 @@
 import asyncio
 
+
 async def hello(i):
     print(f"hello {i} started")
     await asyncio.sleep(4)
     print(f"hello {i} done")
+
 
 async def main():
     task1 = asyncio.create_task(hello(1))
@@ -11,5 +13,6 @@ async def main():
     task2 = asyncio.create_task(hello(2))
     await task1
     await task2
+
 
 asyncio.run(main())  # main loop

e2ea1c2898e0dbe1e5de9b7431ddfea9b3c8e69e
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-26 14:09:54 -0700

    test commit to learn pytorch

--- asyn-task.py
+++ asyn-task.py
@@ -3,20 +3,20 @@ import asyncio
 
 class consumer(object):
 
-	def __init__(self):
-		self.count = 0
-		self.process = asyncio.create_task(self.dispatcher())
-		await self.process
-		
+    def __init__(self):
+        self.count = 0
+        self.process = asyncio.create_task(self.dispatcher())
+        await self.process
+        
 
-	async def scheduler(self):
-		await self.process
+    async def scheduler(self):
+        await self.process
 
-	async def dispatcher(self):
-		print(f'task {self.count} started')
-		await asyncio.sleep(4)
-		print(f'task {self.count} completed')
-		self.count +=1
+    async def dispatcher(self):
+        print(f'task {self.count} started')
+        await asyncio.sleep(4)
+        print(f'task {self.count} completed')
+        self.count +=1
 
 cn = consumer()
 asyncio.run(cn.scheduler())

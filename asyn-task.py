ff1f59f8908ca9c8b934f813307390dc5e3b02e1
Shirish Bahirat <shirishbahirat@comcast.net>
2022-10-26 09:01:15 -0700

    test commit to learn pytorch

---
+++ asyn-task.py
@@ -0,0 +1,14 @@
+import asyncio
+
+
+class consumer(object):
+
+	def __init__(self):
+		self.count = 0
+		self.process = asyncio.create_task(self.dispatcher())
+
+	async def dispatcher(self):
+		print(f'task {self.count} started')
+		await asyncio.sleep(4)
+		print(f'task {self.count} completed')
+		self.count +=1

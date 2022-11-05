9d47c333e9b9e09a3c36d06f8f0044251396f82a
Shirish Bahirat <shirishbahirat@comcast.net>
2022-11-05 11:11:11 -0600

    test commit to learn pytorch

--- tinygrad.py
+++ tinygrad.py
@@ -11,6 +11,7 @@ class Value:
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
 
     def __add__(self, other):
+
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
